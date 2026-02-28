909577bf01720395aaa9f3dd82ca7e140ba9365a
Ahmad Bin Masoodd <ahmabinmasoodd@gmail.com>
2026-02-28 14:02:54 +0500

    Week 1: Console application using custom MathLibrary via NuGet

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ahmad;
+using math_library;
+namespace ahmad
+{
+    class Student
+    {
+        public void Display()
+        {
+            Console.WriteLine("The is the student");
+            double sum=Calculator.sum(4.5, 45.05, 6.6, 3.65);
+            Console.WriteLine($"SUM : {sum}");
+        }
+    }
+    
+}
+namespace ali
+{
+    class Teacher
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Student student = new Student();
+            student.Display();
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("This is the teacher class");
+        }
+        
+    }
+}
---
+++ math_library/math_library/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace math_library
+{
+    public class Calculator
+    {
+        public static double sum(params double[] array)
+        {   
+            double sum = 0;
+            foreach(double number in array)
+            {
+                sum+= number;
+            }
+            return sum;
+        }
+    }
+}
